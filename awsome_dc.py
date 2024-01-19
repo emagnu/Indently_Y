3d46140d4c6d268d2e7f259fafaac3c3101007c0
Emmanuele <emagnubcp@gmail.com>
2024-01-19 15:23:19 +0000

    All working

--- awsome_dc.py
+++ awsome_dc.py
@@ -7,6 +7,7 @@ class Fruit:
     grams: float
     cost_per_kg: float
     is_rare: InitVar[bool] = False
+    similar_fruits: list[str] = field(default_factory=list)
 
     # total_value: float = self.grams / 1000 * self.cost_per_kg
     # Need to create a placeholder -- CALCULATE LATER!
@@ -16,21 +17,27 @@ class Fruit:
         self.total_value = self.grams / 1000 * self.cost_per_kg
 
         if is_rare:
-            self.total_value * 2
+            self.total_value *= 2
+            self.cost_per_kg *= 2
 
 
 def main() -> None:
     apple: Fruit
     apple = Fruit("Apple", 2500, 3)
     banana = Fruit("Banana", 1500, 10, is_rare=True)
+    print(f" --  Banana: {banana}")
+    banana = Fruit("Banana", 1500, 10, is_rare=False)
+    print(f" --  Banana: {banana}")
+    orange: Fruit = Fruit("Orange", 500, 1, similar_fruits=["apple", "lemon"])
+    print(f" --  Orange: {orange}")
 
     print(apple)
     print(apple.grams)
     print(apple.total_value)
     print(banana)
     print(banana.total_value)
+    print(orange.total_value)
 
 
 if __name__ == "__main__":
     main()
-    #
